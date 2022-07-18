9dfd87f93d835e244f91d4511b30a005552ae55c
Kirill Naydovich <kirillnaydovich@gmail.com>
2022-07-18 23:17:34 +0300

    refactor: move "clear storage" button to filters

--- online-store/src/ts/filterView.ts
+++ online-store/src/ts/filterView.ts
@@ -5,20 +5,30 @@ import type Model from './model';
 export default class FilterView {
     rangeFiltersView: RangeFiltersView;
     valueFiltersView: ValueFiltersView;
+    resetLocalStorage: HTMLButtonElement;
 
     constructor(public model: Model, public filterContainer: HTMLElement) {
         this.filterContainer.innerHTML = /* HTML */ `
             <div class="filter__range"></div>
             <div class="filter__value"></div>
-            <input type="reset" class="filter__reset" />
+            <button class="btn__reset">Сбросить фильтры</button>
         `;
 
         const rangeContainer = this.filterContainer.querySelector('.filter__range') as HTMLElement;
         const valueContainer = this.filterContainer.querySelector('.filter__value') as HTMLElement;
-        const reset = this.filterContainer.querySelector('.filter__reset') as HTMLButtonElement;
+        const reset = this.filterContainer.querySelector('.btn__reset') as HTMLButtonElement;
 
         this.rangeFiltersView = new RangeFiltersView(this.model, rangeContainer);
         this.valueFiltersView = new ValueFiltersView(this.model, valueContainer);
+
+        this.resetLocalStorage = document.createElement('button');
+        this.resetLocalStorage.classList.add('btn__reset');
+        this.resetLocalStorage.textContent = 'Очистить LocalStorage';
+
+        this.filterContainer.append(this.resetLocalStorage);
+        this.resetLocalStorage.addEventListener('click', () => {
+            localStorage.removeItem('userSettings');
+        });
 
         reset.addEventListener('click', () => {
             this.model.modelFilters.reset();
