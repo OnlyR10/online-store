b29dac9598466139a5b2dd89a3a86c3b40f30489
Kirill Naydovich <kirillnaydovich@gmail.com>
2022-07-18 15:44:35 +0300

    fear: add the ability to clear the localstorage

--- online-store/src/ts/toolsView.ts
+++ online-store/src/ts/toolsView.ts
@@ -5,11 +5,22 @@ import SortView from './sortView';
 export default class ToolsView {
     searchView: SearchView;
     sortView: SortView;
+    resetLocalStorage: HTMLButtonElement;
 
     constructor(public model: Model, public toolsContainer: HTMLElement) {
         this.searchView = new SearchView(this.model);
         this.sortView = new SortView(this.model);
+
+        this.resetLocalStorage = document.createElement('button');
+        this.resetLocalStorage.classList.add('tools__reset-storage');
+        this.resetLocalStorage.textContent = 'Очистить LocalStorage';
+
+        this.toolsContainer.append(this.resetLocalStorage);
         this.toolsContainer.append(this.searchView.container);
         this.toolsContainer.append(this.sortView.container);
+
+        this.resetLocalStorage.addEventListener('click', () => {
+            localStorage.removeItem('userSettings');
+        });
     }
 }
