fb2a76879c7b55cbae617066c2a577e33bf682cc
Kirill Naydovich <kirillnaydovich@gmail.com>
2022-07-18 15:42:30 +0300

    feat: add busket counter

--- online-store/src/ts/basketView.ts
+++ online-store/src/ts/basketView.ts
@@ -5,24 +5,30 @@ export default class BasketView {
     basketContainerView: BasketContainerView;
     basketContainer: HTMLElement;
     basketWarning: HTMLParagraphElement;
+    basketCounter: HTMLParagraphElement;
 
     constructor(public model: Model, public basket: HTMLElement) {
         this.basket.innerHTML = /* HTML */ `
             <h2 class="basket-header">Корзина</h2>
+            <p class="basket-counter">0</p>
             <p class="basket-warning">Ваша корзина пуста!</p>
             <div class="basket__container"></div>
         `;
 
-        this.basketContainer = this.basket.querySelector('.basket__container') as HTMLElement;
+        this.basketCounter = this.basket.querySelector('.basket-counter') as HTMLParagraphElement;
         this.basketWarning = this.basket.querySelector('.basket-warning') as HTMLParagraphElement;
+        this.basketContainer = this.basket.querySelector('.basket__container') as HTMLElement;
 
         this.basketContainerView = new BasketContainerView(this.model, this.basketContainer);
+
+        this.update();
     }
 
     update() {
         while (this.basketContainer.lastChild) {
             this.basketContainer.lastChild.remove();
         }
+        this.basketCounter.textContent = `${this.model.basket.length}`;
         this.basketWarning.classList.toggle('hidden', Boolean(this.model.basket.length));
         this.basketContainerView.update();
     }
